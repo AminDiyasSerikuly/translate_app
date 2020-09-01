8f68c306852bc40b692689743c98c3e49b9b0fa3
amin <160106014@sdu.stu.edu.kz>
2020-09-01 11:42:35 +0600

    fix bug with layout

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::redirect('/home', '/translation');
+Route::redirect('/', '/translation');
 Route::get('/translation/get', 'TranslationController@getTranslation')->middleware('auth');
 Route::resource('translation', 'TranslationController')->middleware('auth');
 
